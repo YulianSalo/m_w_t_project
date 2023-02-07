87c11b5e1b81eec4d1d44ff5985593f73b40e07c
YulianSalo <39466050+YulianSalo@users.noreply.github.com>
2023-02-08 00:10:35 +0200

    localhost to 0.0.0.0 (#2)

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.get("/", function(req, res) {
 
 //for CORS
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
