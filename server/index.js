ff7aed12bcf9c4e4d29fad90e9c15273f734d00a
yuliansalo <julian_salo@yahoo.com>
2023-02-08 18:57:13 +0200

    f_b_communiction

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ async function main() {
 
   try{
       await mongoose.connect(mongopath, {useNewUrlParser: true, useUnifiedTopology: true});
-      app.listen(port);
+      app.listen(port, '0.0.0.0');
       console.log(`Server is running at port ${port}`);
   }
   catch(err) {
