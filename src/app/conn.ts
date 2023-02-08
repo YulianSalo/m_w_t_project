1a78aae75b19f8f06ed005f76bc0015fee2b1a29
Paul <doesntexist514@gmail.com>
2023-02-08 18:27:42 +0200

    small fixes

--- src/app/conn.ts
+++ src/app/conn.ts
@@ -1,5 +1,5 @@
 export class Conn {
-    static nodeurl:string="http://0.0.0.0:3000";
+    static nodeurl:string="http://localhost:3000";
     static skey:string="$@&*!(#*!(!A!Y!{!*@)!)*#)*#2)!)!#_!_!!@)(#!"
 }
 //static keyword allow us to directly use the variable of the class without making its object
